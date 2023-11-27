7f36a7d7329df2ec9199b14348c493c1e42327b4
Caijunzhi <chickenbilibili@outlook.com>
2023-11-27 20:34:05 +0800

    markdown transfer APIs finished

--- blog/url.py
+++ blog/url.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("blogFlow", views.blogFlow, name="blogFlow"),
     path('blog-avatar/<int:id>/', views.blogAvatar, name='blog_avatar'),
+    path('blog-article/<int:id>/', views.blogArticle, name='blog_article'),
+    path('blog_find_id/<int:id>/', views.blog_find_id, name='blog_find_id'),
 ]
+
+
+# blog_find_id
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import BlogInfo
 from django.http import HttpResponse
 from django.conf import settings
 import os
-
+from django.shortcuts import get_object_or_404
 def blogFlow(request):
     blog_objects = BlogInfo.objects.all()
 
@@ -34,3 +34,35 @@ def blogAvatar(request, id):
         return response
     except IOError:
         return HttpResponse(status=404)
+    
+def blogArticle(request, id):
+    markdown_path = os.path.join(settings.BASE_DIR, 'blog', 'Blogs', str(id), 'article.md')
+
+    try:
+        with open(markdown_path, 'r', encoding='utf-8') as f:
+            markdown_content = f.read()
+        
+        response = HttpResponse(content_type='text/plain')
+        response.write(markdown_content)
+        return response
+    except IOError:
+        return HttpResponse(status=404)
+    
+def blog_find_id(request, id):
+    blog = get_object_or_404(BlogInfo, blog_id=id)
+    
+    # 增加 blog_reads 值
+    blog.blog_reads += 1
+    blog.save()
+
+    blog_dict = {
+        'blog_id': blog.blog_id,
+        'blog_name': blog.blog_name,
+        'blog_likes': blog.blog_likes,
+        'blog_hates': blog.blog_hates,
+        'blog_date': blog.blog_date.strftime('%Y-%m-%d'),
+        'blog_category': blog.blog_category,
+        'blog_description': blog.blog_description,
+        'blog_reads': blog.blog_reads
+    }
+    return JsonResponse(blog_dict)
